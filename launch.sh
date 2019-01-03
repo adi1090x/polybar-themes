a955616da1a60469b1c368d084ea42e539c545e0
Aditya Shakya <adi1090x@gmail.com>
2019-01-03 17:52:59 +0530

    Files Added

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main -c /home/USER/.config/polybar/config &
