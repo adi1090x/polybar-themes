d6a8273b0374f01570cefa25be05014c8c27bcb2
Dennis Z <denniszarger@gmail.com>
2020-03-19 14:35:55 -0400

    Enabled polybar-6 to switch types in-place without erasing user mods.
    I like polybar-6 a lot, but it always erased my config when I switched types.
    
    * Added MODULES-END "tag" to each config-t*.ini
    * Modified scripts/type.sh to only modify between MODULES-MODULES-END of user config.ini

--- polybar-6/scripts/type.sh
+++ polybar-6/scripts/type.sh
@@ -5,40 +5,52 @@
 
 PDIR="$HOME/.config/polybar"
 LAUNCH="polybar-msg cmd restart"
- 
+
+function switchto {
+    # Copy (line 1 to "MODULES") of config.ini to tmp.ini
+    sed -n '1,/MODULES/{/MODULES/d;p;}' $PDIR/config.ini > $PDIR/tmp.ini
+    # Append ("MODULES" to "MODULES-END") of requested type to tmp.ini
+    sed -n '/MODULES/,/MODULES-END/{/MODULES-END/d;p;}' $PDIR/$1 >> $PDIR/tmp.ini
+    # Append (MODULES-END to EOF) of config.ini to tmp.ini
+    sed -n '/MODULES-END/,$p' $PDIR/config.ini >> $PDIR/tmp.ini
+    # Overwrite existing config.ini
+    mv $PDIR/tmp.ini $PDIR/config.ini
+}
+
 if  [[ $1 = "-type-1" ]]; then
 # Copying The File
-cp $PDIR/config-t1.ini $PDIR/config.ini
+switchto "config-t1.ini"
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-2" ]]; then
 # Copying The File
-cp $PDIR/config-t2.ini $PDIR/config.ini
+# cp $PDIR/config-t2.ini $PDIR/config.ini
+switchto "config-t2.ini"
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-3" ]]; then
 # Copying The File
-cp $PDIR/config-t3.ini $PDIR/config.ini
+switchto "config-t3.ini"
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-4" ]]; then
 # Copying The File
-cp $PDIR/config-t4.ini $PDIR/config.ini
+switchto "config-t4.ini"
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-5" ]]; then
 # Copying The File
-cp $PDIR/config-t5.ini $PDIR/config.ini
+switchto "config-t5.ini"
 # Restarting polybar
 $LAUNCH &
 
 elif  [[ $1 = "-type-6" ]]; then
 # Copying The File
-cp $PDIR/config-t6.ini $PDIR/config.ini
+switchto "config-t6.ini"
 # Restarting polybar
 $LAUNCH &
 
