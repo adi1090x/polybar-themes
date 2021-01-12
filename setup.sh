2cf8ce59977f69e0b0d24743d854372ee7d6705a
Tomasz Trębski <kornicameister@gmail.com>
2021-01-12 09:28:33 +0100

    Simplify fonts installation code
    Removes duplicated `cp` instructions and replaces it with single `mkdir` and `cp`

--- setup.sh
+++ setup.sh
@@ -10,12 +10,8 @@ PDIR="$HOME/.config/polybar"
 # Install Fonts
 install_fonts() {
 	echo -e "\n[*] Installing fonts..."
-	if [[ -d "$FDIR" ]]; then
-		cp -rf $DIR/fonts/* "$FDIR"
-	else
-		mkdir -p "$FDIR"
-		cp -rf $DIR/fonts/* "$FDIR"
-	fi
+	[[ ! -d "$FDIR" ]] && mkdir -p "$FDIR"
+	cp -rf $DIR/fonts/* "$FDIR"
 }
 
 # Install Themes
